0d0c956901d63a87fa0d334e99eb02fb1160ffac
davisrr <davisrocha.contato@gmail.com>
2026-08-07 20:03:34 -0300

    Added repository for saving objects.

---
+++ repository/Lista.java
@@ -0,0 +1,10 @@
+package SistemaDeCadastro.repository;
+
+import SistemaDeCadastro.model.Usuario;
+
+import java.util.ArrayList;
+
+public class Lista
+{
+    public static ArrayList<Usuario> usuarios = new ArrayList<>();
+}
