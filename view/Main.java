12ab3420d312ee91edec8634b117bfd2195aaf3d
davisrr <davisrocha.contato@gmail.com>
2026-08-10 10:29:42 -0300

    Added buffer clearing after error.

--- view/Main.java
+++ view/Main.java
@@ -53,7 +53,8 @@ public class Main {
 
                     catch (Exception e)
                     {
-                        throw new RuntimeException(e);
+                        System.out.println("An unexpected error occurred. Please try again.\n");
+                        scanner.nextLine();
                     }
 
                     break;
